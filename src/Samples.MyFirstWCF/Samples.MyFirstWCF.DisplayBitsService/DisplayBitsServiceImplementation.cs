64b6418b0885c769c5a48db519fc49b81ba0c6a3
Martin Ortega <lynxestudio@outlook.com>
2023-12-04 12:24:57 -0600

    Add source code to the project.

---
+++ src/Samples.MyFirstWCF/Samples.MyFirstWCF.DisplayBitsService/DisplayBitsServiceImplementation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Samples.MyFirstWCF.DisplayBitsService
+{
+	public class DisplayBitsServiceImplementation : IDisplayBitsServiceContract
+	{
+		public string DisplayToBits (int i)
+		{
+		int mask = 1 << 31;
+		StringBuilder buf = new StringBuilder (35);
+		for (int bit = 1; bit <= 32; bit++) 
+		{
+		buf.Append((i & mask) == 0 ? '0' : '1');
+		i <<= 1;
+		if(bit % 8 == 0)
+		buf.Append(' ');
+		}
+		return buf.ToString();
+		}
+	}
+}
+
